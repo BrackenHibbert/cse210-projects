20ef11b3c4a001035adf92e8c53927591576f315
BrackenHibbert <hib23001@byui.edu>
2025-01-11 12:44:09 -0700

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        string letterGrade = "";
+
+        if (number >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (number >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (number >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (number >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your Grade in the class is {letterGrade}.");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You Passed the Class! Congratulations.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class, better luck next time!");
+        }
     }
 }
