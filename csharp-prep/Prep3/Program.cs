9440732cad9c2135d453153af521bd877d43dc72
BrackenHibbert <hib23001@byui.edu>
2025-01-15 14:49:32 -0700

    Worked on prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string answer = ("incorrect");
+
+        Console.WriteLine("What is the Magic Number? ");
+            string response = Console.ReadLine();
+            int magicNumber = int.Parse(response);
+
+        while (answer == "incorrect")
+        {
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            int numberGuess = int.Parse(guess);
+
+            if (numberGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            if (numberGuess < magicNumber);
+            {
+                Console.WriteLine("Higher");
+            }
+
+            if (numberGuess == magicNumber);
+            {
+                response = ("correct");
+            }
+        }
+        Console.WriteLine("Correct! You win the game.");
     }
 }
