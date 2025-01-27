dae7c926432c5f5a56a9ff6b21b4c568cd3def3a
BrackenHibbert <hib23001@byui.edu>
2025-01-27 16:17:16 -0700

    in class teaching aid start

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Entry
+{
+    // attributes
+    public string _prompt;
+
+    public string _entryDateTime;
+
+    public string _entryText;
+
+    // behavior
+    public void Display()
+    {
+        Console.WriteLine($"{_entryDateTime} {_prompt}");
+        Console.WriteLine($"{_entryText}");
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        // Console.WriteLine("Hello Develop02 World!");
+        Entry userEntry = new();
+        userEntry._prompt = "How was your day?";
+        userEntry._entryDateTime = "January 27, 2025";
+        userEntry._entryText = "Good.";
+
+        userEntry.Display();
     }
 }
